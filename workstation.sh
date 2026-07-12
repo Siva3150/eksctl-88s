4092bd799122e96e30302e891cb8f856ffc75427
Siva prasad <sivaprasad3150@gmail.com>
2026-07-12 18:05:33 +0530

    k8

--- workstation.sh
+++ workstation.sh
@@ -30,6 +30,12 @@ sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 #k9s 
 curl -sS https://webinstall.dev/k9s | bash
 
+#helm 
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4
+chmod 700 get_helm.sh
+./get_helm.sh
+
+
 # go to ec2-user home
 cd /home/ec2-user
 git clone https://github.com/Siva3150/eksctl-88s.git
